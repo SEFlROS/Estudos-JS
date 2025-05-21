1fec9fa464e4fd6cf4a187879fa2b2d8acb35d99
SEFlROS <joaogabrielpm2003@gmail.com>
2025-05-21 10:09:37 -0300

    Iniciando JavaScript

---
+++ 1_Intro/Js/script.js
@@ -0,0 +1,1 @@
+console.log("Teste console 3")
---
+++ 2_Operadores/js/operadores.js
@@ -0,0 +1,43 @@
+// Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof "-28");
+
+// Operações aritiméticas
+
+console.log(2 - 4);
+console.log(10 + 5);
+console.log(10 / 5);
+console.log(10 * 5);
+
+// Números Especiais
+
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(23 * "as");
+
+console.log(typeof NaN);
+
+// Strings
+
+console.log("Um texto");
+console.log("Mais um texto");
+console.log("13");
+console.log(typeof "Um texto");
+
+console.log("Um texto \nem duas linhas");
+
+console.log("Espaçamento de \t tab");
+
+// Concatenação
+
+console.log("Oi," + " tudo" + " bem?");
+
+// Interpolação
+
+console.log(`A soma de 2 + 2 é: ${2+2}`)
+
+console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`)
+
