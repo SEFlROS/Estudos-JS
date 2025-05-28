dafe6bd7a77077876a27498c23ad170cd67a4d79
SEFlROS <joaogabrielpm2003@gmail.com>
2025-05-28 11:37:19 -0300

    Finalizando estruturas JS

--- 3_Estruturas/Js/estruturas.js
+++ 3_Estruturas/Js/estruturas.js
@@ -57,42 +57,137 @@ console.log(Math.ceil(5, 14));
 
 console.log("teste");
 
-console.error("erro")
+console.error("erro");
 
-console.warn("aviso")
+console.warn("aviso");
 
 // Estrutura de controle
 
-const m = 10
+const m = 10;
 
-if(m > 5) {
-    console.log(`${m} é maior que 5`)
-
+if (m > 5) {
+  console.log(`${m} é maior que 5`);
 }
 
-const user = "João"
+const user = "João";
 
-if(user === "João") {
-    console.log("Olá João")
+if (user === "João") {
+  console.log("Olá João");
 }
 
 // Else
 
-const loggedin = false
+const loggedin = false;
 
-if(loggedin){
-    console.log("Esta altênticado");
-}else{
-        console.log("Não esta autenticado")
-    }
+if (loggedin) {
+  console.log("Esta altênticado");
+} else {
+  console.log("Não esta autenticado");
+}
 
+const q = 10;
+const w = 15;
 
-    const q = 10
-    const w = 15
+if (q > 5 && w > 20) {
+  console.log("Números mais altos");
+} else {
+  console.log("Os números não são mais altos");
+}
 
-    if(q > 5 && w > 20){
-        console.log("Números mais altos");
-    }else{
-        console.log("Os números não são mais altos")
-    }
-    
+// Else If
+
+if (1 > 2) {
+  console.log("Teste");
+} else if (2 > 3) {
+  console.log("Teste 2");
+} else if (5 > 1) {
+  console.log("Agora sim");
+}
+
+const userName = "João Gabriel";
+const userAge = 21;
+
+if (userName === "José") {
+  console.log("Bem vindo José");
+} else if (userName === "João Gabriel" && userAge === 21) {
+  console.log(`Ola João Gabriel, você tem ${userAge} anos de idade`);
+} else {
+  console.log("Nenhuma condição aceita");
+}
+
+// while
+
+let p = 0;
+
+while (p < 5) {
+  console.log(`Repetindo ${p}`);
+  p = p + 1;
+}
+
+// Loop infinito
+
+// let x = 10
+
+// while(x > 5){
+//     console.log(`Imprimindo ${x}`)
+// }
+
+// do while
+
+let o = 10;
+
+do {
+  console.log(`Valor de o: ${o}`);
+  o--;
+} while (o > 1);
+
+// for
+
+for (let t = 0; t < 10; t++) {
+  console.log(`Repetindo algo....${t}`);
+}
+
+let r = 10;
+
+for (r; r > 0; r = r - 1) {
+  console.log(`o ${r} esta diminindo`);
+}
+
+// Break
+
+for (let g = 20; g > 10; g--) {
+  console.log(`o Valor de g é: ${g}`);
+
+  if (g === 15) {
+    console.log(`O g é igual a ${g}`);
+    break;
+  }
+}
+
+// Continue
+
+for (let s = 0; s < 10; s = s + 1) {
+  if (s % 2 === 0) {
+    console.log("Número par!");
+    continue;
+  }
+  console.log(s);
+}
+
+// Switch
+
+const job = "Advogado";
+
+switch (job) {
+  case "Programador":
+    console.log("Você é um Programador!");
+    break;
+  case "Engenheiro":
+    console.log("Você é um Engenheiro");
+    break;
+  case "Advogado":
+    console.log("Você é um Advogado");
+    break;
+  default:
+    console.log("Profissão não encontrada");
+}
