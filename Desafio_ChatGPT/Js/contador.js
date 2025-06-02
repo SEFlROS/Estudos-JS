844312c79cb99cd359490c6e4f4adc107b7d93c0
SEFlROS <joaogabrielpm2003@gmail.com>
2025-06-02 17:42:14 -0300

    Adicionando desafios do ChatGPT

---
+++ Desafio_ChatGPT/Js/contador.js
@@ -0,0 +1,7 @@
+let contador = 0;
+const botao = document.getElementById(`botao`);
+const spanContador = document.getElementById(`contador`);
+botao.addEventListener(`click`, function(){
+    contador = contador +1
+    spanContador.innerText = contador;
+});
