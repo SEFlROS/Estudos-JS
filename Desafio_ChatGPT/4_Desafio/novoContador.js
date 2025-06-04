985583d2f8951333f51767da8414b30b3e810f65
SEFlROS <joaogabrielpm2003@gmail.com>
2025-06-04 12:12:56 -0300

    Atualizando contador novo

--- Desafio_ChatGPT/4_Desafio/novoContador.js
+++ Desafio_ChatGPT/4_Desafio/novoContador.js
@@ -2,7 +2,13 @@ let contador = 0;
 const botao = document.getElementById(`botao`);
 const vezes = document.getElementById(`vezes`);
 const text = document.getElementById(`text`);
+const reset = document.getElementById(`reset`);
 botao.addEventListener(`click`, function () {
   contador = contador + 1;
   text.textContent = `Você clicou ${contador} vezes`;
 });
+
+reset.addEventListener(`click`, function(){
+  contador = 0
+  text.textContent = `Quantas vezes você clicou`
+})
